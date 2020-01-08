5130f7d841a5fbec432a582ad5f2af7b029ab45b
Andy Barrett <apbarret@nsidc.org>
2020-01-08 12:06:42 -0700

    added ocean and sea ice variable lists

--- get_esgf_model_inventory.py
+++ get_esgf_model_inventory.py
@@ -1,6 +1,30 @@
 from pyesgf.search import SearchConnection
 import json
 
+seaice_variables = [
+    'siconc',  # (sea ice concentration %)
+    'sithick',  # (floe ice thickness, for comparisons with observations)
+    'sivol',  # (ice volume [m], *area of grid box gives sea ice volume per grid cell)
+    'siage',  # (sea ice age)
+    'sidconcdyn',  # (% change from dynamics)
+    'sidconcth',  # (% change from thermodyncamics)
+    'sidmassth',  # (mass change from thermodynamics)
+    'sidmassdyn',  # (mass change from dynamics)
+    'sispeed',  # (sea ice speed)
+    'sidivvel',  # (sea ice divergence)
+    'simassacrossline',  # (sea ice mass area flux through straits)
+]
+
+ocean_variables = [
+    'mlotst',  # (ocean mixed layer thickness)
+    'mlotstmax',  # (maximum mixed layer depth per month)
+    'sos',  # (sea surface salinity)
+    'tos',  # (sea surface temperature)
+    'so',  # full depth salinity
+    'to',  # full depth temperature
+]
+
+
 def main():
 
     conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
