22577d78d8d041901bcc1d5c1d6a2a91f5072bcf
Andy Barrett <apbarret@nsidc.org>
2021-07-05 11:58:31 -0600

    add func to return ensemble stats filename

--- cmip6/calc_cmip6_ensemble_mean.py
+++ cmip6/calc_cmip6_ensemble_mean.py
@@ -3,11 +3,23 @@
 import cmip6.load as load
 import cmip6.util as util
 
+
+def get_ensemble_stats_filepath(fp):
+    """Returns new filepath for ensemble stats"""
+    new_name = fp.name.replace(".from_siconc", "_ensemble_stats.from_siconc")
+    return fp.with_name(new_name)
+
+    
 def main(scenario, variable, experiment, verbose=True):
-    if verbose: print(f"Loading {load.arctic_ensemble_filepath(scenario, variable, experiment)}")
+    ensemble_filepath = load.arctic_ensemble_filepath(scenario, variable, experiment)
+    ensemble_stats_filepath = get_ensemble_stats_filepath(ensemble_filepath)
+    
+    if verbose: print(f"Loading {ensemble_filepath}")
     ensemble_df = load.cmip6_ensemble(scenario, experiment, variable)
+    
     ensemble_stats_df = util.get_ensemble_stats(ensemble_df)
     print(ensemble_stats_df)
+    print(ensemble_stats_filepath)
     return
 
 
