f5b65f11739a94dbae4a65612df38b7c0bd76807
Andy Barrett <apbarret@nsidc.org>
2021-07-05 12:36:53 -0600

    add command line args

--- cmip6/calc_cmip6_ensemble_mean.py
+++ cmip6/calc_cmip6_ensemble_mean.py
@@ -29,10 +29,20 @@ def calc_smip6_ensemble_stats(scenario, variable, experiment, verbose=True):
 
 
 if __name__ == "__main__":
-    scenario = "historical"
-    variable = "siextentn"
-    experiment = "r1i1p1f1"
-    verbose=True
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Calculates ensemble statistics")
+    parser.add_argument("scenario", type=str, help="Identifier for scenario")
+    parser.add_argument("variable", type=str, help="variable to process")
+    parser.add_argument("experiment", type=str, help="Identifier for experiment")
+    parser.add_argument("--verbose", "-v", action="store_true")
     
-    calc_smip6_ensemble_stats(scenario, variable, experiment, verbose=verbose)
+    #scenario = "historical"
+    #variable = "siextentn"
+    #experiment = "r1i1p1f1"
+    #verbose=True
+
+    args = parser.parse_args()
+    
+    calc_smip6_ensemble_stats(args.scenario, args.variable, args.experiment, verbose=args.verbose)
     
