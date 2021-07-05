3260ea110581fb91a856a1bac5e68fd12c1bb459
Andy Barrett <apbarret@nsidc.org>
2021-07-05 14:13:33 -0600

    add new scenario

--- cmip6/plot_observerved_and_cmip6_ensemble.py
+++ cmip6/plot_observerved_and_cmip6_ensemble.py
@@ -1,6 +1,9 @@
+import matplotlib.pyplot as plt
+
 import cmip6.load as load
 import cmip6.plotting as plotting
 
+scale = 1e-6
 
 def main(month=9, plotfile='cmip6_and_observed.png', verbose=True):
     """
@@ -13,12 +16,18 @@ def main(month=9, plotfile='cmip6_and_observed.png', verbose=True):
     """
 
     if verbose: print("Loading data")
-    historical = load.cmip6_ensemble("historical", "r1i1p1f1")
-    print(historical)
-    
-    fig, ax = plotting.siextentn_filled(0., historical, 0., '')
+    historical = load.cmip6_ensemble_stats("historical", "r1i1p1f1")
+    historical = historical[historical.index.month == month]
+    historical = historical * scale
 
-    fig.show()
+    ssp370 = load.cmip6_ensemble_stats("ssp370", "r1i1p1f1")
+    ssp370 = ssp370[ssp370.index.month == month]
+    ssp370 = ssp370 * scale
+    
+    if verbose: print("Making plot")
+    fig, ax = plotting.siextentn_filled(0., historical, ssp370, 'SSP370')
+
+    plt.show()
 
     return
 
